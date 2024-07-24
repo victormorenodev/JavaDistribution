a0b19f1f5616ac11476308c07fbe3723ba5e2f02
HenriqueOliveiraGomes <pedhenriqueogomes@gmail.com>
2024-07-24 15:17:28 -0330

    Atulização de distriuicao normal e upando distribuição uniforme

---
+++ DistribuicaoUniforme.java
@@ -0,0 +1,21 @@
+public class DistribuicaoUniforme extends Distribuicao{
+    int a;
+    int b;
+
+    DistribuicaoUniforme(int x, int y){
+        //Construtor recebendo os parâmetros de entrada
+        a = x; b = y;
+    }
+
+    double Calcular(Double x){
+        // usando a função calcular para retornar o valor para a distribuição
+        double resultado;
+        if ((a <= x) && ( x <=b)){
+            resultado = 1/(b-a);
+        }else {
+            resultado = 0;
+        } 
+        return resultado;
+    }
+    
+}
