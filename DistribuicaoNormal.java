7d8ee780c261dc44fbd70c86df7fdceada8abad5
HenriqueOliveiraGomes <pedhenriqueogomes@gmail.com>
2024-07-24 14:46:18 -0330

    priemiro codigo para a distribuicao normal

---
+++ DistribuicaoNormal.java
@@ -0,0 +1,23 @@
+public class DistribuicaoNormal extends Distribuicao{
+    int m;
+    int desvPadrao;
+
+    DistribuicaoNormal(int x, int y){
+        //Construtor recebendo os parâmetros de entrada
+        m = x; 
+        desvPadrao = y;
+    }
+
+    double partUm(double x){
+        double resultado = Math.pow((x - m)/desvPadrao, 2) * 1/2;
+        return resultado;
+    }
+
+    @Override
+    double Calcular(Double x) {
+        //super.Calcular(Double x);
+        double valorDistrib = Math.pow(2.71828, partUm(x))/desvPadrao * Math.sqrt( 2 * 3.14159);
+        return valorDistrib;
+    }
+    
+}
